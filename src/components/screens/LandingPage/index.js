630768ab5875a61042364b93aabd54fd869bbef1
GraceChiamaka <obieri.grace@gmail.com>
2020-11-18 01:55:09 +0100

    Add api setup

--- src/components/screens/LandingPage/index.js
+++ src/components/screens/LandingPage/index.js
@@ -1,53 +1,67 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Row, Col } from 'antd';
 import NewsShot from '../../../assets/images/alucard.jpg';
-
+import { allNewsSource } from '../../../helpers/api';
 import './landing.style.scss';
 
-const LandingPageContent = () => (
-    <div className="landing-page-container">
-        <div className="page-content">
-            <Row justify="space-between">
-                <Col span={1} className="flex-column">
-                    <div className="page-nav left">
-                        <p>Prev story</p>
-                        <h3>Rehabd for ...</h3>
-                    </div>
-                </Col>
-                <Col span={21}>
-                    <div className="center-pad">
-                        <div className="news-sketch">
-                            <div>
-                                <img src={NewsShot} alt="new shot " />
+const LandingPageContent = () => {
+    useEffect(() => {
+        getSources();
+    }, []);
+
+    let getSources = async () => {
+        try {
+            const result = await allNewsSource();
+            console.log(result);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    return (
+        <div className="landing-page-container">
+            <div className="page-content">
+                <Row justify="space-between">
+                    <Col span={1} className="flex-column">
+                        <div className="page-nav left">
+                            <p>Prev story</p>
+                            <h3>Rehabd for ...</h3>
+                        </div>
+                    </Col>
+                    <Col span={21}>
+                        <div className="center-pad">
+                            <div className="news-sketch">
+                                <div>
+                                    <img src={NewsShot} alt="new shot " />
+                                </div>
+                            </div>
+                            <p className="timestamp">01.11.2020</p>
+                            <h1 className="front-page-heading">Rehab for distracted people</h1>
+                            <p className="front-page-description">
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere
+                                voluptates nobis excepturi ipsa tenetur fugit voluptate earum
+                            </p>
+                            <div className="credits">
+                                <p className="">
+                                    <em>Journalist / </em>
+                                    <span>Carl Pirata</span>
+                                </p>
+                                <p className="">
+                                    <em>Photographer / </em>
+                                    <span>Antoine Muti</span>
+                                </p>
                             </div>
                         </div>
-                        <p className="timestamp">01.11.2020</p>
-                        <h1 className="front-page-heading">Rehab for distracted people</h1>
-                        <p className="front-page-description">
-                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere
-                            voluptates nobis excepturi ipsa tenetur fugit voluptate earum
-                        </p>
-                        <div className="credits">
-                            <p className="">
-                                <em>Journalist / </em>
-                                <span>Carl Pirata</span>
-                            </p>
-                            <p className="">
-                                <em>Photographer / </em>
-                                <span>Antoine Muti</span>
-                            </p>
+                    </Col>
+                    <Col span={2} className="flex-column">
+                        <div className="page-nav next">
+                            <p>next story</p>
+                            <h3>Rehabd for ...</h3>
                         </div>
-                    </div>
-                </Col>
-                <Col span={2} className="flex-column">
-                    <div className="page-nav next">
-                        <p>next story</p>
-                        <h3>Rehabd for ...</h3>
-                    </div>
-                </Col>
-            </Row>
+                    </Col>
+                </Row>
+            </div>
         </div>
-    </div>
-);
+    );
+};
 
 export default LandingPageContent;
---
+++ src/helpers/api.js
@@ -0,0 +1,49 @@
+import axios from 'axios';
+import { loadState } from '../store/LocalStorage';
+
+const baseUrl = 'https://newsapi.org/v2';
+
+export const getToken = () => {
+    return loadState();
+};
+const apiKey = process.env.REACT_APP_NEWS_API_KEY;
+
+export const makeApiCall = (axiosConfigObj) => {
+    const config = {
+        ...axiosConfigObj,
+        baseURL: baseUrl,
+    };
+    return new Promise((resolve, reject) => {
+        return axios(config)
+            .then((res) => {
+                if (res.status !== 200) {
+                    return reject(res.data);
+                }
+                return resolve(res.data);
+            })
+            .catch((err) => {
+                const { response, request, message } = err;
+                const error = response
+                    ? response.message
+                    : request
+                    ? 'Network error, please try again later'
+                    : message;
+                console.log(error, '===');
+                return reject(error);
+            });
+    });
+};
+
+export const allNewsSource = () => {
+    return new Promise((resolve, reject) => {
+        try {
+            const sourcesList = makeApiCall({
+                url: `/sources?apiKey=${apiKey}`,
+                method: 'get',
+            });
+            return resolve(sourcesList);
+        } catch (error) {
+            return reject(error);
+        }
+    });
+};
